8bcac965a54c3a119144679e1db7166cc99e78b3
Benhur <benhuratwork@gmail.com>
2024-12-20 09:58:33 +0530

    str py

--- 2py.py
+++ 2py.py
@@ -10,3 +10,19 @@ def strpresent():
         return -1
 print(strpresent())
 
+#LEET28
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(len(haystack)-len(needle)+1):
+            if needle==haystack[i:len(needle)]:
+                return i
+        else:
+            return -1
+
+        
